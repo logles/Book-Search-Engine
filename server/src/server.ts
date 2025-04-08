b9fae9fb2f53eec6c2508356cbcd6aaa9e1962ab
logles <lydialogles@gmail.com>
2025-04-07 19:11:06 -0600

    define dir

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,13 @@ import typeDefs from "./schemas/typeDefs.js";
 import resolvers from "./schemas/resolvers.js";
 import { getUserFromToken } from "./services/auth.js";
 
+//this code fixes the _dir error in deployment
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app: Application = express();
 const PORT = process.env.PORT || 3001;
 
